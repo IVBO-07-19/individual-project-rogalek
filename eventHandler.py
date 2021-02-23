5bdb3a357bd3a6ebf780ce5014222234c0acae2a
Miki_san <mikhael.rykhlov@gmail.com>
2021-02-23 22:04:04 +0300

    Version 0.0.2: Working at menu screen and basic physics engine

--- eventHandler.py
+++ eventHandler.py
@@ -2,10 +2,19 @@ import sys
 
 
 class EventHandler:
-    def __init__(self):
-        pass
+    def __init__(self, game, menu):
+        self.game = game
+        self.menu = menu
 
-    def setInputs(self, events, pg):
+    def setInputs(self, events, pg, mouse):
+
         for e in events:
             if e.type == pg.QUIT:
                 sys.exit()
+            if e.type == pg.MOUSEBUTTONDOWN and not self.game:
+
+                # if the mouse is clicked on the
+                # button the game is terminated
+                if 1280 / 2 <= mouse[0] <= 1280 / 2 + 140 and 720 / 2 <= mouse[1] <= 720 / 2 + 40:
+                    self.game = True
+                    self.menu = False
